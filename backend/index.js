fec81d00964629e844362ccfd3866b3d2b3b9836
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2024-12-14 18:45:19 +0530

    database conection and env created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 const app = express();
 
 // dummy get request
@@ -19,7 +22,8 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running at port ${PORT}`);
 });
