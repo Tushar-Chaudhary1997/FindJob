1e6adc9d94eb8732741c7e9cd0df39d6ad09e564
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2024-12-17 11:51:48 +0530

    get all job completed

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -52,3 +52,28 @@ export const postJob = async (req, res) => {
     console.log(error);
   }
 };
+
+export const getAllJobs = async (req, res) => {
+  try {
+    const keyword = req.query.keyword || "";
+    const query = {
+      $or: [
+        { title: { $regex: keyword, $options: "i" } },
+        { description: { $regex: keyword, $options: "i" } },
+      ],
+    };
+    const jobs = await Job.find(query);
+    if (!jobs) {
+      return res.status(400).json({
+        message: "Jobs not found",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      jobs,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
