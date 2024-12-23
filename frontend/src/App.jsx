3c2025235c85645d6db2408b370741530d842201
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2024-12-23 16:14:13 +0530

    update tailwind confg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  <>
-    <h1 className="text-red-500">Lets build job title</h1>
-  </>;
+  return (
+    <>
+      <h1 className="text-red-500">Lets build job title</h1>
+    </>
+  );
 }
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {
       borderRadius: {
