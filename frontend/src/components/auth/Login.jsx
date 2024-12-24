4a5773bbfb94e904da3fe842e19044d2e03877da
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2024-12-24 23:18:27 +0530

    login created

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
@@ -6,7 +6,7 @@ import { Button } from "../ui/button";
 import { RadioGroup } from "../ui/radio-group";
 import { Link } from "react-router-dom";
 
-const Signup = () => {
+const Login = () => {
   const [input, setInput] = useState({
     fullname: "",
     email: "",
@@ -29,19 +29,13 @@ const Signup = () => {
           action=""
           className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
         >
-          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
-          <div className="my-2">
-            <Label>Full Name</Label>
-            <Input type="text" placeholder="Tushar" />
-          </div>
+          <h1 className="font-bold text-xl mb-5">Login</h1>
+
           <div className="my-2">
             <Label>Email</Label>
             <Input type="text" placeholder="Tushar@gmail.com" />
           </div>
-          <div className="my-2">
-            <Label>Phone Number</Label>
-            <Input type="text" placeholder="+91" />
-          </div>
+
           <div className="my-2">
             <Label>Password</Label>
             <Input type="password" placeholder="Password" />
@@ -67,18 +61,14 @@ const Signup = () => {
                 <Label htmlFor="r2">Recruiter</Label>
               </div>
             </RadioGroup>
-            <div className="flex items-center gap-2">
-              <label>Profile</label>
-              <Input accept="image/*" type="file" className="cursor-pointer" />
-            </div>
           </div>
           <Button type="submit" className="w-full my-4">
-            Signup
+            Login
           </Button>
           <span className="text-sm">
-            Already have an account?
-            <Link to="/login" className="text-blue-600">
-              Login
+            Don't have an account?
+            <Link to="/signup" className="text-blue-600">
+              Signup
             </Link>
           </span>
         </form>
@@ -87,4 +77,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Login;
