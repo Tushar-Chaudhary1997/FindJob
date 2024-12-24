00c210373794e14e9462167a324e5eb135c7973c
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2024-12-24 19:30:45 +0530

    signup completed

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { RadioGroup } from "../ui/radio-group";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
@@ -31,21 +33,40 @@ const Signup = () => {
             <Input type="password" placeholder="Password" />
           </div>
           <div className="flex items-center justify-between">
-            <RadioGroup defaultValue="comfortable">
+            <RadioGroup className="flex items-center gap-4 my-5">
               <div className="flex items-center space-x-2">
-                <RadioGroupItem value="default" id="r1" />
-                <Label htmlFor="r1">Default</Label>
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Student</Label>
               </div>
               <div className="flex items-center space-x-2">
-                <RadioGroupItem value="comfortable" id="r2" />
-                <Label htmlFor="r2">Comfortable</Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="compact" id="r3" />
-                <Label htmlFor="r3">Compact</Label>
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
               </div>
             </RadioGroup>
+            <div className="flex items-center gap-2">
+              <label>Profile</label>
+              <Input accept="image/*" type="file" className="cursor-pointer" />
+            </div>
           </div>
+          <Button type="submit" className="w-full my-4">
+            Signup
+          </Button>
+          <span className="text-sm">
+            Already have an account?
+            <Link to="/login" className="text-blue-600">
+              Login
+            </Link>
+          </span>
         </form>
       </div>
     </div>
