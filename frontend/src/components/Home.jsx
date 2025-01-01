86302440c188e86d8b8c2e05264ec732c85dd0f4
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2025-01-01 18:44:57 +0530

    new feature added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./shared/Navbar";
+import HeroSection from "./HeroSection";
+import CategoryCarousal from "./CategoryCarousal";
+import LatestJobs from "./LatestJobs";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1>Welcome Home</h1>
+      <HeroSection />
+      <CategoryCarousal />
+      <LatestJobs />
+      {/* <Footer /> */}
     </div>
   );
 };
