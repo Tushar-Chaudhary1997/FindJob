43f5fd728e4d1799bc68ec269be6b6a7a89a3a7c
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2025-01-03 00:33:28 +0530

    job cards updated

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -3,23 +3,25 @@ import React from "react";
 
 const LatestJobCards = () => {
   return (
-    <div className="">
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer">
       <div>
-        <h1>Company Name</h1>
-        <p>India</p>
+        <h1 className="font-medium text-lg">Company Name</h1>
+        <p className="text-sm text-gray-500">India</p>
       </div>
       <div>
-        <h1>Job Title</h1>
-        <p>jkdbsjkdsbdsjbsd dskjbdskjbds jdkssdb</p>
+        <h1 className="font-bold text-lg my-2">Job Title</h1>
+        <p className="text-sm text-gray-600">
+          jkdbsjkdsbdsjbsd dskjbdskjbds jdkssdb
+        </p>
       </div>
-      <div>
+      <div className="flex items-center gap-2 mt-4">
         <Badge className="text-blue-700 font-bold" variant="ghost">
           12 Positions
         </Badge>
-        <Badge className="text-blue-700 font-bold" variant="ghost">
+        <Badge className="text-[#F83002] font-bold" variant="ghost">
           Part Time
         </Badge>
-        <Badge className="text-blue-700 font-bold" variant="ghost">
+        <Badge className="text-[#7209b7] font-bold" variant="ghost">
           24LPA
         </Badge>
       </div>
