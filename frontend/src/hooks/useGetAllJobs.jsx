91b4bb02ef6fad994533569e5903445adf783273
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2025-01-04 17:13:46 +0530

    code updated

---
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+const useGetAllJobs = () => {
+  useEffect(()=>{
+    const fetchAllJobs = async ()=>{
+      try{
+        const 
+      } catch(){
+        console
+      }
+    }
+  })
+}
+
+export default useGetAllJobs
