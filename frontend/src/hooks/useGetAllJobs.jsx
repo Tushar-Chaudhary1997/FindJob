596e543999e5ab4768523a7c1fd3bc5f564e78f4
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2025-01-05 12:59:50 +0530

    getAllJobs created

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -1,15 +1,26 @@
-import React, { useEffect } from 'react'
+import axios from "axios";
+import { useEffect } from "react";
+import { JOB_API_END_POINT } from "@/utils/constant";
+import { useDispatch } from "react-redux";
+import { setAllsJobs } from "@/redux/jobSlice";
 
 const useGetAllJobs = () => {
-  useEffect(()=>{
-    const fetchAllJobs = async ()=>{
-      try{
-        const 
-      } catch(){
-        console
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchAllJobs = async () => {
+      try {
+        const res = await axios.get(`${JOB_API_END_POINT}/get`, {
+          withCredentials: true,
+        });
+        if (res.data.success) {
+          dispatch(setAllsJobs(res.data.jobs));
+        }
+      } catch (error) {
+        console.log(error);
       }
-    }
-  })
-}
+    };
+    fetchAllJobs();
+  }, []);
+};
 
-export default useGetAllJobs
+export default useGetAllJobs;
