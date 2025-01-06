a3b447ef362a3b1affb6beb219ec2544bbfb65d6
Tushar-Chaudhary1997 <tusharchaudhary804@gmail.com>
2025-01-06 22:55:04 +0530

    code modified

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import { Badge } from "./ui/badge";
 import { useNavigate } from "react-router-dom";
 
-const Job = () => {
+const Job = ({ job }) => {
   const navigate = useNavigate();
   const jobId = "hjsdjdjds";
   return (
@@ -23,31 +23,28 @@ const Job = () => {
           </Avatar>
         </Button>
         <div>
-          <h1 className="font-medium text-lg">Company Name</h1>
+          <h1 className="font-medium text-lg">{job?.company?.name}</h1>
           <p className="text-sm text-gray-500">India</p>
         </div>
       </div>
       <div>
-        <h1 className="font-bold text-lg my-2">Title</h1>
-        <p className="text-sm text-gray-600">
-          lorem20 sddshkbds sehdvdsk hewvldss dshv sdhjvsd sdhjsjdsa hjsdhvkds
-          hjdvshjves hsvadks hjvsddkas hdasvdsh hvdsvsjadvhda
-        </p>
+        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
+        <p className="text-sm text-gray-600">{job?.description}</p>
       </div>
       <div className="flex items-center gap-2 mt-4">
         <Badge className="text-blue-700 font-bold" variant="ghost">
-          12 Positions
+          {job?.position} Positions
         </Badge>
         <Badge className="text-[#F83002] font-bold" variant="ghost">
-          Part Time
+          {job?.jobType}
         </Badge>
         <Badge className="text-[#7209b7] font-bold" variant="ghost">
-          24LPA
+          {job?.salary}LPA
         </Badge>
       </div>
       <div className="flex items-center gap-4 mt-4">
         <Button
-          onClick={() => navigate(`/description/${jobId}`)}
+          onClick={() => navigate(`/description/${job?._id}`)}
           variant="outline"
         >
           Details
